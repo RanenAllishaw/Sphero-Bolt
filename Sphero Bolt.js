a9a3017d1e94e10117921eab615d0380bfc7186f
Ranen Allishaw <ranens.work@gmail.com>
2023-02-10 08:32:15 -0500

    FEB.10.2023

--- Sphero Bolt.js
+++ Sphero Bolt.js
@@ -1,14 +1,12 @@
 // Programmer: Ranen Allishaw
 // Date: 02.06.2023
 // Project: Sphero Bolt Testing
-// Version: 1.3
+// Version: 1.5
 // Bolt: 183F
-
+5
 
 /**
- * Hello World Practice Program
- * LED turns to Maroon
- * Forward fo 2 seconds at 60 speed
+ * Says Ello Moto
 */
 async function elloMmmmmmoto() {
  await speak("Ello,,,,,,                                   MMMMMMoto!", true);
@@ -16,14 +14,27 @@ async function elloMmmmmmoto() {
 
 
 /**
- * ninetyDegree function goes forward for a specified amount of time and speed
+ * Roomrun function goes forward for a specified amount of time and speed
  * pausing, turning 90 degrees clockwise then moving forward for a specific amount
  * of speed and time
 */
-async function ninetyDegree() {
+async function Roomrun() {
 	await roll((getHeading() + 0), 600, 1.25); // Heading 0 degrees at 600 speed for 1.25 seconds
 	await delay(1);
-	await roll((getHeading() + 95), 900, 4); // heading 90 degrees at 300 speed for 2 seconds
+	await roll((getHeading() + 90), 900, 3.5); // Heading 90 degrees at 900 speed for 3.5 seconds
+	await delay(1);
+	await roll((getHeading() + 92.5), 900, 1.65); // Heading 90 degrees at 900 speed for 1.65 seconds
+	await delay(1);
+	await roll((getHeading() + 90), 900, 6.05); // Heading 90 degrees at 900 speed for 6.05 sconds
+	await delay(1);
+	await roll((getHeading() + 90), 900, 4.2);// Heading 90 degrees at 900 speed for 4.2 seconds
+	await delay(1);
+	await roll((getHeading() + 90), 900, 2.8);// Heading 90 degrees at 900 speed for 2.8 seconds
+	await delay(1);
+	await roll((getHeading() + 90), 900, 1.5);// Heading 90 degrees at 900 speed for 1.5 seconds
+	await delay(1);
+	await roll((getHeading() + 270), 900, 3.2);// Heading 90 degrees at 900 speed for 3.2 seconds
+
 }
 
 
@@ -31,9 +42,9 @@ async function ninetyDegree() {
  * startProgram function sends the program to your Sphero Bolt
 */
 async function startProgram() {
-	setMainLed({ r: 40, g: 5, b: 5});
-	await Sound.Animal.Monkey.play(true)
-	await ninetyDegree() // This is calling the ninetyDegree function so that it will run
+	setMainLed({ r: 40, g: 0, b: 255});
+	await Sound.Animal.play(true)
+	await Roomrun() // This is calling the ninetyDegree function so that it will run
 	await elloMmmmmmoto() // This is calling the helloWorld function so that it will run
 }
 
