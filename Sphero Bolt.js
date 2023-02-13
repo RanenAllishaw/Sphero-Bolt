bba62c3507843e2591dde91a39baf3c545c7ca6e
Ranen Allishaw <ranens.work@gmail.com>
2023-02-13 07:50:19 -0500

    Initial Labyrinth Competition commit

--- Sphero Bolt.js
+++ Sphero Bolt.js
@@ -1,5 +1,6 @@
 /*
-Programmer: Ranen Allishaw
-Date: 02.06.2023
-Project: Sphero Bolt Testing
+Programming Team: Ranen Allishaw & Kyle Rocco
+Date: FEB.13.2023
+Project: Labyrinth Competition
+Version: 1.0
 */
