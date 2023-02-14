dc9b10e27c8a96b27ad64cdb2ad5ca0e193d7b95
Ranen Allishaw <ranens.work@gmail.com>
2023-02-14 08:39:03 -0500

    RED

--- Sphero Bolt.js
+++ Sphero Bolt.js
@@ -2,7 +2,7 @@
 Programming Team: Ranen Allishaw & Kyle Rocco
 Date: FEB.13.2023
 Project: Labyrinth Competition
-Version: 2.4
+Version: 3.3
 Bolt: 183F
 */
 
@@ -25,14 +25,24 @@ async function CheckpointBLUE() {
  *
 */
 async function CheckpointNoise_1(){
-  await roll(90, 64, 1.5);
+  await roll(90, 70, 1.5);
   await delay(2);
-  await roll(180, 59, 1);
+  await roll(180, 65, 1);
   await spin(42.5, 2.5);
   await Sound.Animal.Monkey.play();
 }
 
+
+async function CheckpointRED() {
+	await roll(217.5, 53.75, 1.5);
+	await delay(2);
+	await roll(132.5, 39.1, 1.1);
+	setMainLed({ r: 255, g: 0, b: 0})
+}
+
+
 async function startProgram() {
-await CheckpointBLUE(); // Calls CheckpointBLUE
-await CheckpointNoise_1();
+	await CheckpointBLUE(); // Calls CheckpointBLUE
+	await CheckpointNoise_1();
+	await CheckpointRED();
 }
