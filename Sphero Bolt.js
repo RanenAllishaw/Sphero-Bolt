afc4edcc5f26cf217462d2947407abe04ac5c42e
Ranen Allishaw <ranens.work@gmail.com>
2023-02-17 08:40:28 -0500

    PURPLE & Noise #3

--- Sphero Bolt.js
+++ Sphero Bolt.js
@@ -2,7 +2,7 @@
 Programming Team: Ranen Allishaw & Kyle Rocco
 Date: FEB.15.2023
 Project: Labyrinth Competition
-Version: 5.2
+Version: 7.2
 Bolt: 183F
 */
 
@@ -15,7 +15,7 @@ Bolt: 183F
 */
 async function CheckpointBLUE() {
   await scrollMatrixText('START', { r: 255, g: 255, b: 255 }, 30, true);
-  await roll(0, 78.5, 2);
+  await roll(0, 78, 2);
   await spin(90, 2.5);
   setMainLed({ r: 0, g: 0, b: 255 })
 }
@@ -25,11 +25,11 @@ async function CheckpointBLUE() {
  *
 */
 async function CheckpointNoise_1(){
-  await roll(90, 69.5, 1.5);
+  await roll(90, 68, 1.5);
   await delay(2);
-  await roll(180, 65, 1);
+  await roll(180, 66.25, 1);
   await spin(42.5, 2.5);
-  await Sound.Animal.Monkey.play();
+  await Sound.Animal.play();
 }
 
 
@@ -37,11 +37,11 @@ async function CheckpointNoise_1(){
  *
 */
 async function CheckpointRED() {
-	await roll(217.5, 56.75, 1.5);
-	await delay(2);
-	await roll(132.5, 41, 1.1);
-	setMainLed({ r: 255, g: 0, b: 0})
-	await delay(2);
+    await roll(217.5, 56.1, 1.5);
+    await delay(2);
+    await roll(132.5, 40.75, 1.1);
+    setMainLed({ r: 255, g: 0, b: 0})
+    await delay(2);
 }
 
 
@@ -49,10 +49,10 @@ async function CheckpointRED() {
  *
 */
 async function CheckpointNoise_2() {
-	await roll(42.5, 55.75, 1.5);
-	await delay(1.5);
-	await roll(130, 40.5, 1.75);
-	await Sound.Animal.Monkey.play();
+    await roll(42.5, 55.75, 1.5);
+    await delay(1.5);
+    await roll(130, 35.75, 1.75);
+    await Sound.Animal.play();
 }
 
 
@@ -60,11 +60,12 @@ async function CheckpointNoise_2() {
  *
 */
 async function CheckpointGREEN() {
-	await delay(1.5);
-	await roll(180, 43.75, 1);
-	await delay(1.5);
-	await roll(89.5, 41, 1.1);
-	setMainLed({ r: 0, g: 255, b: 0})
+    await delay(1.5);
+    await roll(180, 43.5, 1);
+    await delay(1.5);
+    setMainLed({ r: 0, g: 255, b: 0})
+    await roll(89.5, 29 , 1.1);
+
 }
 
 
@@ -72,13 +73,24 @@ async function CheckpointGREEN() {
  *
 */
 async function CheckpointNoise_3() {
-	await roll(89.5, 41, 1.1);
+    await roll(89.5, 40, 1.1);
+    await delay(2);
+    await roll(0, 45, 1.5);
+    await delay(2);
+    await roll(315, 46, 1.25);
+    await Sound.Animal.play();
+    await delay(2);
+}
+
+
+/**
+ *
+*/
+async function CheckpointPURPLE() {
+	await roll(0, 60, 1.5);
+	setMainLed({ r: 40, g: 0, b: 255})
 	await delay(2);
-	await roll(0, 45, 1.5);
-	await delay(2);
-	await roll(315, 45, 1.25);
-	await Sound.Animal.Monkey.play();
-	await delay(2);
+	await scrollMatrixText('FINISH', { r: 255, g: 255, b: 255 }, 30, true);
 }
 
 
@@ -86,10 +98,11 @@ async function CheckpointNoise_3() {
  *
 */
 async function startProgram() {
-	await CheckpointBLUE(); // Calls CheckpointBLUE
-	await CheckpointNoise_1();
-	await CheckpointRED();
-	await CheckpointNoise_2();
-	await CheckpointGREEN();
-	await CheckpointNoise_3();
+    await CheckpointBLUE(); // Calls CheckpointBLUE
+    await CheckpointNoise_1();
+    await CheckpointRED();
+    await CheckpointNoise_2();
+    await CheckpointGREEN();
+    await CheckpointNoise_3();
+	await CheckpointPURPLE();
 }
